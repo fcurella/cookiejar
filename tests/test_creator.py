2e9046ea8bf70df730aee86d31febf770e58d123
Flavio Curella <flavio.curella@gmail.com>
2013-11-26 09:07:44 -0600

    don't pollute user's home dir when testing

--- tests/test_creator.py
+++ tests/test_creator.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from unittest import TestCase
 
 from cookiejar.channel import Channel
@@ -7,8 +8,11 @@ from cookiejar.settings import SettingsReader
 
 class ChannelTests(TestCase):
     def test_add(self):
-        config_path = os.path.join((os.path.dirname(os.path.abspath(__file__))), 'cookiejarrc')
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+
+        config_path = os.path.join(current_dir, 'cookiejarrc')
         settings = SettingsReader(config_file=config_path)
+        settings['templates_dir'] = os.path.join(current_dir, 'cookiecutters')
         index = os.path.join((os.path.dirname(os.path.abspath(__file__))), 'index.1.json')
 
         channel = Channel(settings=settings, index=index)
@@ -21,3 +25,4 @@ class ChannelTests(TestCase):
         channel.remove("audreyr/pypackage")
         destination_path = os.path.join(settings['templates_dir'], 'audreyr', 'pypackage')
         self.assertFalse(os.path.exists(destination_path))
+        shutil.rmtree(settings['templates_dir'])
